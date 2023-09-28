84b03d28fd819bf5f2a42cb30ed626627ecedfa0
Kristjan Jansen <kristjan.jansen@gmail.com>
2023-09-28 22:07:41 +0300

    ast

---
+++ pages/template.vue
@@ -0,0 +1,20 @@
+<script setup>
+const content = ref(`# Hello world
+`);
+import { parse as parseSlides } from "@slidev/parser";
+
+const markdownContent = computed(() => compileMarkdown(content.value));
+const ast = computedAsync(async () => parseSlides(content.value));
+</script>
+
+<template>
+  <div class="grid grid-cols-2 fixed inset-0">
+    <textarea
+      class="overflow-auto bg-gray-800 text-gray-200 font-mono p-5 outline-none"
+      v-model="content"
+    ></textarea>
+    <div class="p-4 overflow-auto font-mono whitespace-pre-wrap">
+      {{ ast }}
+    </div>
+  </div>
+</template>
