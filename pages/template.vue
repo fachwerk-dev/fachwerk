391211006b184e71cb002ed308a94a0e30ff413b
Kristjan Jansen <kristjan.jansen@gmail.com>
2023-10-04 22:35:00 +0300

    work with marked

--- pages/template.vue
+++ pages/template.vue
@@ -1,6 +1,5 @@
 <script setup>
-const content = ref(`# Hello world
-`);
+const content = ref(`# Hello world`);
 import { parse as parseSlides } from "@slidev/parser";
 
 const markdownContent = computed(() => compileMarkdown(content.value));
@@ -8,11 +7,14 @@ const ast = computedAsync(async () => parseSlides(content.value));
 </script>
 
 <template>
-  <div class="grid grid-cols-2 fixed inset-0">
+  <div class="grid grid-cols-3 fixed inset-0">
     <textarea
       class="overflow-auto bg-gray-800 text-gray-200 font-mono p-5 outline-none"
       v-model="content"
     ></textarea>
+    <div class="p-4 overflow-auto font-mono whitespace-pre-wrap bg-gray-300">
+      {{ ast }}
+    </div>
     <div class="p-4 overflow-auto font-mono whitespace-pre-wrap">
       {{ ast }}
     </div>
