ce379c769ec8e26241b6a70fec84a3950d9eaf5c
Kristjan Jansen <kristjan.jansen@gmail.com>
2023-10-10 23:07:39 +0300

    hm

--- pages/template.vue
+++ pages/template.vue
@@ -50,19 +50,54 @@ const parseContent = (content) => {
   const a = content.replace(/^---/g, "").split(pageSeparator);
   let prev = null;
   let pages = [];
+  let current = 0;
   a.forEach((c) => {
     const prevLines = prev?.trim().split(/\r?\n/g).length;
-
+    const content = parseSection(c);
+    const numOfSections = content.length;
+    const lengthOfSections = content
+      .map((c) => c.contentLines)
+      .reduce((a, b) => a + b, 0);
+    const numOfSectionFrontmatters = content.filter(
+      (c) => c.frontmatter
+    ).length;
+    const lengthOfSectionFrontmatters = content
+      .map((c) => c.frontmatterLines)
+      .reduce((a, b) => (a || 0) + (b || 0), 0);
+    const pageLength =
+      numOfSections +
+      lengthOfSections +
+      numOfSectionFrontmatters * 2 +
+      lengthOfSectionFrontmatters; // TODO: consider prevLines / frontmatter
+    const start = current;
+    const end = start + pageLength;
     if (prev?.replace(yamlSeparator, "").includes(":")) {
       pages.push({
         frontmatter: prev.includes(":") ? prev : null,
         frontmatterLines: prevLines,
-        content: parseSection(c),
+        content,
+        // numOfSections,
+        // lengthOfSections,
+        // numOfSectionFrontmatters,
+        // lengthOfSectionFrontmatters,
+        // pageLength,
+        start,
+        end,
       });
     } else {
-      pages.push({ content: parseSection(c) });
+      pages.push({
+        content,
+        // numOfSections,
+        // lengthOfSections,
+        // numOfSectionFrontmatters,
+        // lengthOfSectionFrontmatters,
+        // pageLength,
+        start,
+        end,
+      });
     }
     prev = c;
+    current = end + 1;
   });
   return pages.filter((p) => p.content.length > 0);
 };
@@ -80,22 +115,32 @@ const parsedContent = computed(() => {
     />
 
     <div class="flex flex-col gap-4 p-4">
-      <div
-        class="p-4 flex flex-col gap-4 bg-gray-100"
-        v-for="page in parsedContent"
-      >
-        <div
-          v-if="page.frontmatter"
-          class="font-mono text-sm whitespace-pre mb-2"
-        >
-          {{ page.frontmatter }}
-        </div>
-        <div class="border p-4 bg-gray-200" v-for="section in page.content">
-          <div class="font-mono text-sm whitespace-pre mb-2">
-            {{ section.frontmatter }}
+      <div class="flex gap-2">
+        <div>
+          <div class="text-right" v-for="(_, row) in content.split('\n')">
+            {{ row }}
           </div>
-          <div class="prose" v-html="parse(section.content)" />
         </div>
+        <div class="font-mono whitespace-pre-line">
+          {{ content }}
+        </div>
+        <!-- <div
+          class="p-4 flex flex-col gap-4 bg-gray-100"
+          v-for="page in parsedContent"
+        >
+          <div
+            v-if="page.frontmatter"
+            class="font-mono text-sm whitespace-pre mb-2"
+          >
+            {{ page.frontmatter }}
+          </div>
+          <div class="border p-4 bg-gray-200" v-for="section in page.content">
+            <div class="font-mono text-sm whitespace-pre mb-2">
+              {{ section.frontmatter }}
+            </div>
+            <div class="prose" v-html="parse(section.content)" />
+          </div>
+        </div> -->
       </div>
     </div>
     <div class="p-4 overflow-auto font-mono text-sm whitespace-pre-wrap">
