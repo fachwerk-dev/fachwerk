d2f4093bb2474982c01a785bd2887f1759b8e1d6
Kristjan Jansen <Kristjan.Jansen@energia.ee>
2023-10-20 21:13:59 +0300

    range fixes

--- utils/range.js
+++ utils/range.js
@@ -5,20 +5,8 @@
  * @param {number} [end] - The end of the range (exclusive).
  * @param {number} [step=1] - The difference between each successive number in the range.
  * @returns {number[]} An array of numbers in the specified range.
- *
- * @example
- * // Returns [0, 1, 2, 3, 4]
- * range(5);
- *
- * @example
- * // Returns [5, 6, 7, 8, 9]
- * range(5, 10);
- *
- * @example
- * // Returns [0, 2, 4, 6, 8]
- * range(0, 10, 2);
  */
-function range(start, end, step = 1) {
+export function range(start, end, step = 1) {
   if (end === undefined) {
     end = start;
     start = 0;
