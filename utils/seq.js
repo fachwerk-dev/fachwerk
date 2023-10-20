62a8d430a39bce6b0f1c6cf023f14017dc39b731
Kristjan Jansen <Kristjan.Jansen@energia.ee>
2023-10-20 22:32:50 +0300

    fix

--- utils/seq.js
+++ utils/seq.js
@@ -4,14 +4,14 @@
  * @function
  * @name seq
  * @param {number} length - The number of values to generate. Must be a positive integer.
- * @param {number} [start=0] - The starting value of the sequence.
  * @param {number} [step=1] - The step value by which each subsequent number in the sequence will differ.
+ * @param {number} [start=0] - The starting value of the sequence.
  * @returns {number[]} An array of numbers, starting from `start`, incrementing by `step` for each subsequent number, until there are `length` numbers in the array.
  */
-export function seq(x, y = 0, z = 1) {
+export function seq(length, step = 1, start = 0) {
   const result = [];
-  for (let i = 0; i < x; i++) {
-    result.push(y + i * z);
+  for (let i = 0; i < length; i++) {
+    result.push(start + i * step);
   }
   return result;
 }
