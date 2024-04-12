83620061ec3e16c9d84e50bce84c79314988f47d
Kristjan Jansen <kristjan.jansen@gmail.com>
2024-04-12 10:25:58 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-16 flex flex-col gap-4">
     <a class="underline" href="https://main-preview.fachwerk.dev">Main site preview</a>
-    <a class="underline" href="https://corporate-preview.fachwerk.dev">Corporate site preview</a>
+    <a class="underline" href="https://corporate-preview.fachwerk.dev">Corporate site preview (todo)</a>
     <a class="underline" href="https://industry-preview.fachwerk.dev">Industry site preview (todo)</a>
   </div>
 </template>
